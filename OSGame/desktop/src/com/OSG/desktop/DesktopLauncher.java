c50d90c6a73103f973142e13ca1bd1ca64fae1a8
Brett Menzies <scimor5@gmail.com>
2015-03-05 10:02:40 -0800

    Bootstrapped gradle project

---
+++ OSGame/desktop/src/com/OSG/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.OSG.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.OSG.OSGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OSGame(), config);
+	}
+}
