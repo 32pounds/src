9db110ec8028e26bebbb2947340311d778bebb46
Brett Menzies <scimor5@gmail.com>
2015-03-05 18:32:15 -0800

    Added drawable interface

---
+++ OSGame/core/src/com/renderer/Drawable.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface Drawable{
+	void	draw(SpriteBatch batch);
+	boolean isExpired();
+}
