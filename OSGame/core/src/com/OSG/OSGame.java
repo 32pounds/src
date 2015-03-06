103c8dc44978bbe26a742768f52bb68087a96147
jordlynn <snake.river52@gmail.com>
2015-03-05 18:12:37 -0800

    Yellow test

--- OSGame/core/src/com/OSG/OSGame.java
+++ OSGame/core/src/com/OSG/OSGame.java
@@ -14,14 +14,18 @@ public class OSGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		batch.draw(img, 50, 50);
+		
+		
 		batch.end();
 	}
 }
