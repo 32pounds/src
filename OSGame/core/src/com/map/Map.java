8e1868d2b4c5022b525faf51fc1d3a22be1f5440
michael <michael@Vera>
2015-03-05 19:47:11 -0800

    beginning of the map class, will work more later.

--- OSGame/core/src/com/map/Map.java
+++ OSGame/core/src/com/map/Map.java
@@ -10,14 +10,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Map implements Drawable{
 	
+    char grid[][];
+    
         public Map()
         {
-            char grid[][];
+            public static final int XDIMENSION=32;
+            public static final int YDIMENSION=32;
+            //create the grid with the map
+            Texture floor= new Texture("BeigeTile.png");
 	}
         
 	public void draw(SpriteBatch batch)
         {
-		for()
+		//for(int i=0; i<grid.getX; ++i)
+                    //for(int j=0; j<grid.getY; ++j)
+                    //{
+                       //if(grid[x][y]=="x")
+                            //batch.draw(floor, 
+                    //}
 	}
         
 	public boolean isExpired(){
