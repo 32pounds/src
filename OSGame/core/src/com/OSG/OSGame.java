b7a820f22270c829727f62a393e01e67d1848d89
michael <michael@Vera>
2015-03-05 18:15:07 -0800

    testing by making black background

--- OSGame/core/src/com/OSG/OSGame.java
+++ OSGame/core/src/com/OSG/OSGame.java
@@ -19,7 +19,7 @@ public class OSGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		
