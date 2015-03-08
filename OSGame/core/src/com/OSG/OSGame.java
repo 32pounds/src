cec92eeb70447a8e4aaa80ae5fb3295abd2d3bef
dora0426 <dora0426@vandals.uidaho.edu>
2015-03-08 13:29:38 -0700

    Game Loop thread

--- OSGame/core/src/com/OSG/OSGame.java
+++ OSGame/core/src/com/OSG/OSGame.java
@@ -2,6 +2,7 @@ package com.OSG;
 
 import com.renderer.Drawable;
 import com.map.Map;
+import com.gameloop.GameLoop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -12,21 +13,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class OSGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+        GameLoop thread;
 
 	Drawable map;
-
+        
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		map = new Map();
+                thread = new GameLoop();
+                thread.setRunning(true);
+                thread.start();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+		
+                
+                batch.begin();
+                thread.run();
 
 		map.draw(batch);
 		//batch.draw(img, 50, 50);
