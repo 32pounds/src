e7d1d82821fb948c7a505c568107b6cef22b45f7
jordlynn <snake.river52@gmail.com>
2015-03-09 17:21:21 -0700

    initial commit

---
+++ OSGame/core/src/com/UI/UserInterface.java
@@ -0,0 +1,14 @@
+package com.UI;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public interface UserInterface{
+	
+	void CreateUserInteface();
+}
