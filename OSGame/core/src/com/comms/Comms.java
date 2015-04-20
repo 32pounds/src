600545ff2d514ca0a1d860dda26a8d5f1784aa88
jordlynn <snake.river52@gmail.com>
2015-04-20 09:33:18 -0700

    Comms class file, I'm not sure where this came from

---
+++ OSGame/core/src/com/comms/Comms.java
@@ -0,0 +1,39 @@
+package com.comms;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+public class Comms{
+   // GameState localState;
+
+    // Client networking commands.
+   public void Transmit(Command commandData){
+        
+   }
+
+  // public void SyncWith(GameState gs){
+
+   //}
+
+   public void RequestNewPlayer( Command requestPlayer ){
+
+   }
+   
+   private void Receive(){
+   
+   }
+   
+   // Server networking commands.
+   public void SendGameState(){
+   
+   }
+   
+   public void GetPendingCommand(){
+   
+   }
+
+   
+}
+
