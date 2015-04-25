ab86ae1cf0f3ae598e4e4baa7e0acf567cd0cff4
LizHernandez <hern9899@vandals.uidaho.edu>
2015-04-25 13:40:19 -0700

    Update Map.java
    Altered symbol interpretation to include cyber sprites.

--- OSGame/core/src/com/map/Map.java
+++ OSGame/core/src/com/map/Map.java
@@ -27,13 +27,13 @@ public class Map extends Drawable {
                     case ' ':
                         batch.draw(SpriteStorage.getInstance().getTexture("BeigeTile"), x * XDIMENSION, y * YDIMENSION);
                         break;
-                    case 'X':
+                    case 'B':
                         batch.draw(SpriteStorage.getInstance().getTexture("BlackCarpet"), x * XDIMENSION, y * YDIMENSION);
                         break;
-                    case '^':
+                    case 'o':
                         batch.draw(SpriteStorage.getInstance().getTexture("TransportUp"), x * XDIMENSION, y * YDIMENSION);
                         break;
-                    case 'v':
+                    case 'g':
                         batch.draw(SpriteStorage.getInstance().getTexture("TransportDown"), x * XDIMENSION, y * YDIMENSION);
                         break;
                     case '@':
@@ -98,6 +98,45 @@ public class Map extends Drawable {
                         break;
                     case '/':
                         batch.draw(SpriteStorage.getInstance().getTexture("CautionStripe"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case ',':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberFloor"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case 'H':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberWall"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case 'h':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CorruptedWall"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '#':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberFloor"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case 'x':
+                        batch.draw(SpriteStorage.getInstance().getTexture("Xbox"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case 'X':
+                        batch.draw(SpriteStorage.getInstance().getTexture("Xboxes"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '^':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberUp"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case 'v':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberDown"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '<':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberLeft"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '>':
+                        batch.draw(SpriteStorage.getInstance().getTexture("CyberRight"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '*':
+                        batch.draw(SpriteStorage.getInstance().getTexture("Asterisk"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '!':
+                        batch.draw(SpriteStorage.getInstance().getTexture("Exclamation"), x * XDIMENSION, y * YDIMENSION);
+                        break;
+                    case '$':
+                        batch.draw(SpriteStorage.getInstance().getTexture("Funds"), x * XDIMENSION, y * YDIMENSION);
                         break;
                 }
             }
